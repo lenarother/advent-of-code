24f0d40005a7c7d98fe0a21a1d88e3ddc756f434
Magdaena Rother <rother.magdalena@gmail.com>
2021-10-24 10:15:29 +0200

    Add basic cookiecutter for advent of code katas

---
+++ cookiecutter-aoc/{{ cookiecutter.day }}/solution.py
@@ -0,0 +1,15 @@
+"""{{cookiecutter.title}}
+
+https://adventofcode.com/{{cookiecutter.year}}/day/9
+
+"""
+
+
+def solve(data):
+    return data
+
+
+if __name__ == '__main__':
+    input_data = open('input_data.txt').read()
+    result = solve(input_data)
+    print(f'Example1: {result}')
