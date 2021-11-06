fc2be9a0ed20e5b33a5182acfcf45925c4f9397a
Magdaena Rother <rother.magdalena@gmail.com>
2021-11-06 23:23:32 +0100

    Refactor day 2016 15

--- adventofcode_2016/day_15/solution.py
+++ adventofcode_2016/day_15/solution.py
@@ -4,6 +4,7 @@ https://adventofcode.com/2016/day/15
 
 """
 import re
+from itertools import count
 
 DISC = r'Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+).'
 
@@ -13,24 +14,22 @@ def disc_generator(data):
         yield int(n), int(positions), int(start_position)
 
 
-def is_open(n, positions, start_position, t):
+def is_disc_open(n, positions, start_position, t):
     current_position = start_position + n + t
     return abs(positions - current_position) % positions == 0
 
 
 def check_time(data, t):
     return all([
-        is_open(n, positions, start_position, t)
+        is_disc_open(n, positions, start_position, t)
         for n, positions, start_position in disc_generator(data)
     ])
 
 
 def solve(data):
-    t = 0
-    while True:
+    for t in count():
         if check_time(data, t):
             return t
-        t += 1
 
 
 if __name__ == '__main__':
