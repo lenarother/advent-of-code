c058887ee1a2e4f3ba83cb9a6fdfea4c4c4dc0b1
Magdaena Rother <rother.magdalena@gmail.com>
2021-11-16 20:45:54 +0100

    Solve day 22

--- adventofcode_2016/day_22/solution.py
+++ adventofcode_2016/day_22/solution.py
@@ -5,22 +5,28 @@ https://adventofcode.com/2016/day/22
 """
 import itertools
 import re
-from collections import deque
+from collections import OrderedDict, deque
+from copy import deepcopy
 from functools import reduce
-
 
 DISC = (
     r'/dev/grid/node-x(?P<x>\d+)-y(?P<y>\d+)\s+'
     r'(?P<Size>\d+)T\s+(?P<Used>\d+)T\s+'
     r'(?P<Avail>\d+)T\s+(?P<Use>\d+)%'
 )
+DISC_NULL = r'/dev/grid/node-x(\d+)-y(\d+)\s+\d+T\s+0T\s+\d+T\s+0%'
+DISC_X = r'-x(\d+)-'
 
 
 def parse_discs(data):
-    discs = {}
+    discs = OrderedDict()
     pattern = re.compile(DISC)
     for d in pattern.finditer(data):
-        temp_d = {k: int(v) for k, v in d.groupdict().items() if k in ['x', 'y', 'Used', 'Avail']}
+        temp_d = {
+            k:
+                int(v) for k, v in d.groupdict().items()
+                if k in ['x', 'y', 'Used', 'Avail']
+        }
         x, y = temp_d.pop('x'), temp_d.pop('y')
         discs[(x, y)] = temp_d
     return discs
@@ -49,46 +55,119 @@ def count_viable_pairs(data):
         0
     )
 
-def count_viable_pairs(data):
-    discs = parse_discs(data)
-    return reduce(
-        lambda count, p: count + check_pair(discs[p[0]], discs[p[1]]),
-        get_discs_combinations(discs),
-        0
-    )
-
 
 def solve(data):
     return count_viable_pairs(data)
 
 
-def mark_target(grid):
-    target = (max([k[0] for k in grid]), 0)
-    for k, v in grid.items():
-        if k == target:
-            v.update({'Target': True})
-        v.update({'Target': False})
+class Grid:
+
+    def __init__(self, data=None):
+        self.width = None
+        self.null = None
+        self.str_data = None
+        self.moves_count = 0
+        if data:
+            self.parse(data)
+
+    def __repr__(self):
+        return self.str_data
+
+    def __hash__(self):
+        return (self.str_data)
+
+    def __len__(self):
+        return len(self.str_data)
+
+    @staticmethod
+    def get_null_coord(data):
+        return tuple(map(int, re.findall(DISC_NULL, data)[0]))
+
+    @staticmethod
+    def get_width(data):
+        return max(map(int, set(re.findall(DISC_X, data)))) + 1
+
+    def parse(self, data):
+        self.width = self.get_width(data)
+        null_coord = self.get_null_coord(data)
+        self.null = null_coord[0] + (null_coord[1] * self.width)
+        g_position = (self.width - 1, 0)
+        data_dict = parse_discs(data)
+        null_disc = data_dict[null_coord]
+        self.str_data = ''
+        for y in range(int(len(data_dict)/self.width)):
+            for x in range(self.width):
+                if (x, y) == null_coord:
+                    ch = '_'
+                elif (x, y) == g_position:
+                    ch = 'G'
+                elif is_pair_viable(data_dict[(x, y)], null_disc):
+                    ch = '.'
+                else:
+                    ch = '#'
+                self.str_data += ch
+
+    def is_completed(self):
+        return self.str_data[0] == 'G'
+
+    def is_move_possible(self, m):
+        return 0 <= m < len(self) and self.str_data[m] in '.G'
+
+    def get_possible_moves(self):
+        moves = [
+            self.null - self.width,  # U
+            self.null - 1 if self.null % self.width != 0 else None,  # L
+            (
+                self.null + 1
+                if self.null % self.width != (self.width - 1)
+                else None
+            ),  # R
+            self.null + self.width,  # D
+        ]
+        for m in moves:
+            if m is not None and self.is_move_possible(m):
+                yield m
+
+    def apply_move(self, m):
+        ch = self.str_data[m]
+        self.str_data = self.str_data.replace('_', ch)
+        self.str_data = self.str_data[:m] + '_' + self.str_data[m + 1:]
+        self.null = m
+        self.moves_count += 1
+
+
+def copy_grid(g):
+    g_copy = Grid()
+    g_copy.null = g.null
+    g_copy.width = g.width
+    g_copy.str_data = deepcopy(g.str_data)
+    g_copy.moves_count = g.moves_count
+    return g_copy
 
 
 def access_data(data):
-    return 7
-    grid = parse_grid(data)
-    mark_target(grid)
-
-    candidates = deque([(0, grid)])
+    grid = Grid(data)
+    visited = set()
+    visited.add(str(grid))
+    candidates = deque([grid])
     while candidates:
-        i, grid = candidates.popleft()
-        if grid[0][0] == target:
-            return i
-        for grid in get_adjacent_moves(grid):
-            candidates.append(i + 1, grid)
-
-class Grid:
-
-    def __init__(self, data):
+        grid = candidates.popleft()
+        for m in grid.get_possible_moves():
+            new_grid = copy_grid(grid)
+            new_grid.apply_move(m)
+            if str(new_grid) not in visited:
+                if new_grid.is_completed():
+                    return new_grid.moves_count
+                visited.add(str(new_grid))
+                candidates.append(new_grid)
+    return -1
 
 
 if __name__ == '__main__':
     input_data = open('input_data.txt').read()
     result = solve(input_data)
     print(f'Example1: {result}')
+
+    input_data = open('input_data.txt').read()
+    result = access_data(input_data)
+    print(f'Example2: {result}')
--- adventofcode_2016/day_22/test_solution.py
+++ adventofcode_2016/day_22/test_solution.py
@@ -41,8 +41,7 @@ Filesystem              Size  Used  Avail  Use%
      ),
 )
 
-EXAMPLE_ACCESS_DATA = (
-    ("""
+GRID = """
 Filesystem            Size  Used  Avail  Use%
 /dev/grid/node-x0-y0   10T    8T     2T   80%
 /dev/grid/node-x0-y1   11T    6T     5T   54%
@@ -52,25 +51,38 @@ Filesystem            Size  Used  Avail  Use%
 /dev/grid/node-x1-y2   11T    7T     4T   63%
 /dev/grid/node-x2-y0   10T    6T     4T   60%
 /dev/grid/node-x2-y1    9T    8T     1T   88%
-/dev/grid/node-x2-y2    9T    6T     3T   66%""", 7),
+/dev/grid/node-x2-y2    9T    6T     3T   66%
+"""
+
+GRID_NULL_POSITION = (
+    (GRID, (1, 1)),
 )
 
-EXAMPLE_ACCESS_DATA = (
-    ("""
-Filesystem            Size  Used  Avail  Use%
-/dev/grid/node-x0-y0   10T    8T     2T   80%
-/dev/grid/node-x0-y1   11T    6T     5T   54%
-/dev/grid/node-x0-y2   32T   28T     4T   87%
-/dev/grid/node-x1-y0    9T    7T     2T   77%
-/dev/grid/node-x1-y1    8T    0T     8T    0%
-/dev/grid/node-x1-y2   11T    7T     4T   63%
-/dev/grid/node-x2-y0   10T    6T     4T   60%
-/dev/grid/node-x2-y1    9T    8T     1T   88%
-/dev/grid/node-x2-y2    9T    6T     3T   66%""",
-     """
-..G
-._.
-#.."""),
+GRID_NULL = (
+    (GRID, 4),
+)
+
+GRID_WIDTH = (
+    (GRID, 3),
+)
+
+GRID_STR = (
+    (GRID, '..G._.#..'),
+)
+
+GRID_POSSIBLE_MOVES = (
+    ('..G._.#..', 3, [1, 3, 5, 7]),
+    ('_.G...#..', 3, [1, 3]),
+    ('..G_..#..', 3, [0, 4]),
+    ('._G...#..', 3, [0, 2, 4]),
+)
+
+GRID_APPLY_MOVE = (
+    (GRID, 1, '._G...#..'),
+)
+
+GRID_ACCESS_DATA = (
+    (GRID, 7),
 )
 
 
@@ -84,6 +96,44 @@ def test_parse(data, expected):
     assert parse_discs(data) == expected
 
 
-@pytest.mark.parametrize('data,expected', EXAMPLE_ACCESS_DATA)
-def test_grid(data, expected):
-    assert f'{Grid(data)}' == expected
+@pytest.mark.parametrize('data,expected', GRID_NULL_POSITION)
+def test_get_null_position(data, expected):
+    assert Grid.get_null_coord(data) == expected
+
+
+@pytest.mark.parametrize('data,expected', GRID_NULL)
+def test_grid_null(data, expected):
+    g = Grid(data)
+    assert g.null == expected
+
+
+@pytest.mark.parametrize('data,expected', GRID_WIDTH)
+def test_get_width(data, expected):
+    assert Grid.get_width(data) == expected
+
+
+@pytest.mark.parametrize('data,expected', GRID_STR)
+def test_grid_repr(data, expected):
+    g = Grid(data)
+    assert f'{g}' == expected
+
+
+@pytest.mark.parametrize('data,width,expected', GRID_POSSIBLE_MOVES)
+def test_grid_possible_moves(data, width, expected):
+    g = Grid()
+    g.str_data = data
+    g.null = list(data).index('_')
+    g.width = width
+    assert list(g.get_possible_moves()) == expected
+
+
+@pytest.mark.parametrize('data,move,expected', GRID_APPLY_MOVE)
+def test_apply_move(data, move, expected):
+    g = Grid(data)
+    g.apply_move(move)
+    assert str(g) == expected
+
+
+@pytest.mark.parametrize('data,expected', GRID_ACCESS_DATA)
+def test_access_data(data, expected):
+    assert access_data(data) == expected
