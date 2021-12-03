1c4197a3d1a51269a38863c178a050c2506be9b8
Magdaena Rother <rother.magdalena@gmail.com>
2021-12-03 17:28:44 +0100

    Impruve tests

--- adventofcode_2021/day_03/test_solution.py
+++ adventofcode_2021/day_03/test_solution.py
@@ -8,7 +8,13 @@ from .solution import (
     reverse_bitstring,
     solve,
     solve2,
+    transpose_list,
 )
+
+DATA_SMALL = """
+00100
+11110
+10110"""
 
 DATA = """
 00100
@@ -25,12 +31,31 @@ DATA = """
 01010"""
 
 EXAMPLES = (
+    (DATA_SMALL, '10110'),
     (DATA, '10110'),
 )
 
-EXAMPLES2 = (
+EXAMPLES_SOLVE = (
+    (DATA_SMALL, 198),
     (DATA, 198),
 )
+
+EXAMPLES_REVERSE = (
+    ('0', '1'),
+    ('00000', '11111'),
+    ('11111', '00000'),
+    ('10110', '01001'),
+)
+
+EXAMPLES_TRANSPOSE = (
+    (['11', '22', '33'], [['1', '2', '3'], ['1', '2', '3']]),
+    ([['1', '1'], ['2', '2'], ['3', '3']], [['1', '2', '3'], ['1', '2', '3']]),
+)
+
+
+@pytest.mark.parametrize('data,expected', EXAMPLES_TRANSPOSE)
+def test_transpose_list(data, expected):
+    assert transpose_list(data) == expected
 
 
 @pytest.mark.parametrize('data,expected', EXAMPLES)
@@ -39,12 +64,14 @@ def test_find_most_common(data, expected):
     assert find_most_common_bits(data) == expected
 
 
-@pytest.mark.parametrize('data,expected', EXAMPLES)
+@pytest.mark.parametrize('data,expected', EXAMPLES_REVERSE)
 def test_reverse_bitstring(data, expected):
-    assert reverse_bitstring('10110') == '01001'
+    result = reverse_bitstring(data)
+    assert result == expected
+    assert reverse_bitstring(result) == data  # round trip
 
 
-@pytest.mark.parametrize('data,expected', EXAMPLES2)
+@pytest.mark.parametrize('data,expected', EXAMPLES_SOLVE)
 def test_solve(data, expected):
     assert solve(data) == expected
 
