3ef6c598ecaab1e7adc02f64653f01ed4aaf7268
Magdaena Rother <rother.magdalena@gmail.com>
2021-12-07 08:12:35 +0100

    WIP: solve day 7 2021

---
+++ adventofcode_2021/day_07/solution.py
@@ -0,0 +1,54 @@
+"""Day 7: The Treachery of Whales
+
+https://adventofcode.com/2021/day/7
+
+"""
+
+from collections import defaultdict, Counter
+from functools import reduce
+
+FUEL_COST = {}
+
+def parse(data):
+    return Counter(list(map(int, data.strip().split(','))))
+
+
+def count_fuel(data_dict, position):
+    return sum([(abs(k - position) * v) for k, v in data_dict.items()])
+
+
+def solve(data):
+    result = {}
+    data = parse(data)
+    for position in data:
+        result[position] = count_fuel(data, position)
+    return min(list(result.values()))
+
+
+def count_step(current_position, target):
+    d = abs(current_position - target)
+    if d not in FUEL_COST:
+        FUEL_COST[d] = sum(list(range(d+1)))
+    return FUEL_COST[d]
+
+
+def count_fuel2(data_dict, position):
+    return sum([(count_step(k, position) * v) for k, v in data_dict.items()])
+
+
+def solve3(data):
+    data = parse(data)
+    result = {}
+    for x in range(min(data.keys()), max(data.keys()) + 1):
+        result[x] = count_fuel2(data, x)
+    return min(list(result.values()))
+
+
+if __name__ == '__main__':
+    input_data = open('input_data.txt').read()
+    result = solve(input_data)
+    print(f'Example1: {result}')
+
+    input_data = open('input_data.txt').read()
+    result = solve3(input_data)
+    print(f'Example2: {result}')
---
+++ adventofcode_2021/day_07/test_solution.py
@@ -0,0 +1,44 @@
+import pytest
+
+from .solution import solve, parse, count_fuel, count_step,solve2, solve3
+
+EXAMPLES = (
+    ('16,1,2,0,4,2,7,1,2,14', 37),
+)
+
+EXAMPLES2 = (
+    ('16,1,2,0,4,2,7,1,2,14', 168),
+)
+
+EXAMPLES_COUNT_FUEL = (
+    ('16,1,2,0,4,2,7,1,2,14', 1, 41),
+    ('16,1,2,0,4,2,7,1,2,14', 2, 37),
+    ('16,1,2,0,4,2,7,1,2,14', 3, 39),
+    ('16,1,2,0,4,2,7,1,2,14', 10, 71),
+)
+
+EXAMPLES_COUNT_STEP = (
+    (16, 5, 66),
+    (1, 5, 10),
+    (2, 5, 6),
+)
+
+# @pytest.mark.parametrize('data,expected', EXAMPLES)
+# def test_solve(data, expected):
+#     assert solve(data) == expected
+#
+#
+# @pytest.mark.parametrize('data,position,expected', EXAMPLES_COUNT_FUEL)
+# def test_count_fuel(data, position, expected):
+#     data = parse(data)
+#     assert count_fuel(data, position,) == expected
+#
+#
+# @pytest.mark.parametrize('current_position,target,expected', EXAMPLES_COUNT_STEP)
+# def test_count_step(current_position, target, expected):
+#     assert count_step(current_position, target) == expected
+
+
+@pytest.mark.parametrize('data,expected', EXAMPLES2)
+def test_solve2(data, expected):
+    assert solve3(data) == expected
