f5b64c7b52cea98bfb4ee357466844c49880c4e1
Magdaena Rother <rother.magdalena@gmail.com>
2022-06-08 12:15:20 +0200

    Day 24 2017

---
+++ adventofcode_2017/day_24/solution.py
@@ -0,0 +1,85 @@
+"""Day 24: Electromagnetic Moat
+
+https://adventofcode.com/2017/day/24
+
+"""
+from collections import defaultdict
+
+
+def parse(data):
+    components = defaultdict(set)
+
+    data = data.strip()
+    if data:
+        for line in data.strip().split('\n'):
+            a, b = list(map(int, line.split('/')))
+            components[a].add((a, b))
+            components[b].add((a, b))
+
+    return components
+
+
+def calculate_strength(bridge):
+    _, components, strength = bridge
+    return strength or sum(sum(port) for port in components)
+
+
+def get_valid_components(bridge, components):
+    """Get possible next components as a list."""
+    port, current_components, _ = bridge
+    return [
+        c for c in components.get(port, [])
+        if c not in current_components
+    ]
+
+
+def get_extended_bridge(bridge, component):
+    current_port, current_elements, _ = bridge
+    port1, port2 = component
+    port = port2 if port1 == current_port else port1
+    elements = list(current_elements)
+    elements.append(component)
+    return port, frozenset(elements), 0
+
+
+def get_stronger_bridge(best_bridge, bridge):
+    best_bridge_strength = calculate_strength(best_bridge)
+    bridge_strength = calculate_strength(bridge)
+    bridge = (bridge[0], bridge[1], bridge_strength)
+    return bridge if bridge_strength > best_bridge_strength else best_bridge
+
+
+def get_longer_bridge(best_bridge, bridge):
+    best_bridge_length = len(best_bridge[1])
+    bridge_length = len(bridge[1])
+    if bridge_length == best_bridge_length:
+        return get_stronger_bridge(best_bridge, bridge)
+    return bridge if bridge_length > best_bridge_length else best_bridge
+
+
+def solve(data, get_better_bridge=get_stronger_bridge):
+    components = (parse(data))
+
+    bridges = set()
+    best_bridge = (0, frozenset(), 0)  # port, components, strength
+    bridges.add(best_bridge)
+
+    while bridges:
+        bridge = bridges.pop()
+        valid_components = get_valid_components(bridge, components)
+        for component in valid_components:
+            new_bridge = get_extended_bridge(bridge, component)
+            bridges.add(new_bridge)
+        if not valid_components:
+            best_bridge = get_better_bridge(best_bridge, bridge)
+
+    return calculate_strength(best_bridge)
+
+
+if __name__ == '__main__':
+    input_data = open('input_data.txt').read()
+    result = solve(input_data)
+    print(f'Example1: {result}')
+
+    result = solve(input_data, get_longer_bridge)
+    print(f'Example2: {result}')
---
+++ adventofcode_2017/day_24/test_solution.py
@@ -0,0 +1,46 @@
+import pytest
+
+from .solution import get_longer_bridge, solve
+
+DATA = """
+0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+"""
+
+
+@pytest.mark.parametrize(
+    'data,expected',
+    (
+        ('', 0),
+        ('2/2\n2/3', 0),
+        ('0/2\n2/2', 6),
+        ('0/2\n2/2', 6),
+        ('0/2\n2/3\n2/10', 14),
+        ('0/2\n2/2\n2/10', 18),
+        (DATA, 31),
+    )
+)
+def test_solve(data, expected):
+    assert solve(data) == expected
+
+
+@pytest.mark.parametrize(
+    'data,expected',
+    (
+        ('', 0),
+        ('2/2\n2/3', 0),
+        ('0/2\n2/2', 6),
+        ('0/2\n2/2', 6),
+        ('0/2\n2/3\n2/10', 14),
+        ('0/2\n2/2\n2/10', 18),
+        (DATA, 19),
+    )
+)
+def test_solve2(data, expected):
+    assert solve(data, get_longer_bridge) == expected
