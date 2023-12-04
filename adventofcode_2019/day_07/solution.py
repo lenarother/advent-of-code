6831acee5a92981fdf2d7cf8539f2882116f2e8a
Magdalena Rother <rother.magdalena@gmail.com>
2023-12-04 18:55:45 +0100

    WIP

---
+++ adventofcode_2019/day_07/solution.py
@@ -0,0 +1,15 @@
+"""Day 7: Amplification Circuit
+
+https://adventofcode.com/2019/day/7
+
+"""
+
+
+def solve(data):
+    return data
+
+
+if __name__ == '__main__':
+    input_data = open('input_data.txt').read()
+    result = solve(input_data)
+    print(f'Example1: {result}')
