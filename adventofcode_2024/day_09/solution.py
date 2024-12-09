fd24025ffba04bdd6be37a097542672004ad1164
Magdalena Rother <magdalena.rother@thermondo.de>
2024-12-09 20:52:51 +0100

    Refactor

--- adventofcode_2024/day_09/solution.py
+++ adventofcode_2024/day_09/solution.py
@@ -3,13 +3,11 @@
 https://adventofcode.com/2024/day/9
 
 """
-
 def data_gen(data):
     for i in data.strip():
         yield int(i)
 
-
-def find_first_empty_position(disc, n):
+def find_first_empty_location(disc, n):
     """First for a first available dot.
 
     Start looking from n, not from beginning.
@@ -20,121 +18,125 @@ def find_first_empty_position(disc, n):
         n += 1
 
 def parse_data_to_dict(data):
+    """Create disc map
+
+    Parse data to a dictionary.
+    disc[position] = partition_id or '.' (if empty)
+
+    Contains one key for each disc position
+    """
     disc = dict()
+    disc_location = 0
+    partition_id = 0
+    for counter, partition_size in enumerate(data.strip()):
+        is_occupied = counter % 2 == 0
+        for _ in range(0, int(partition_size)):
+            disc[disc_location] = partition_id if is_occupied else '.'
+            disc_location += 1
+        if is_occupied:
+            partition_id += 1
+    return disc
 
-    for counter, partition in enumerate(data.strip()):
-        for _ in range(0, int(partition)):
-            pass
+def move_data(disc):
+    new_location = 0
+    disc_location_from_end = len(disc) - 1
+    while disc_location_from_end:
+        partition_id = disc[disc_location_from_end]
+        if partition_id != '.':
+            new_location = find_first_empty_location(disc, new_location)
+            if new_location <= disc_location_from_end:
+                disc[new_location] = partition_id
+                disc[disc_location_from_end] = '.'
+        disc_location_from_end -= 1
 
-def solve(data):
-    disc = {}
-    counter = 0
-    id_counter = 0
-    for e, i in enumerate(data_gen(data)):
-        #print(e, i)
-        j = i
-        while j:
-            if e % 2 == 0:
-                disc[counter] = id_counter
-            else:
-                disc[counter] = '.'
-            j -= 1
-            counter += 1
-        if e % 2:
-            id_counter += 1
-
-    position = 0
-    counter -= 1
-    while counter:
-        v = disc[counter]
-        if v != '.':
-            position = find_first_empty_position(disc, position)
-            if position <= counter:
-                disc[position] = v
-                disc[counter] = '.'
-        counter -= 1
-
-    # calculate checksum
+def calculate_checksum(disc):
     checksum = 0
     for k, v in disc.items():
         if v == '.':
             return checksum
         checksum += k * v
-
     return checksum
 
+def solve(data):
+    disc = parse_data_to_dict(data)
+    move_data(disc)
+    return calculate_checksum(disc)
+
+### Second Part
+
+def parse_data_to_dict_2(data):
+    """Create disc map.
+
+    Parse data to a dictionary.
+    disc[position] = (partition_id or '.', partition_size)
+
+    Contains one key for each partition.
+    Keys are not continous.
+    """
+    disc: dict[int, tuple[int|str, int]] = {}
+    occupied_partition_locations: list[int] = []
+    partition_id = 0
+    disc_location = 0
+    for counter, partition_size in enumerate(data_gen(data)):
+        is_occupied = counter % 2 == 0
+        if is_occupied:
+            disc[disc_location] = (partition_id, partition_size)
+            partition_id += 1
+            occupied_partition_locations.append(disc_location)
+        else:
+            disc[disc_location] = ('.', partition_size)
+        disc_location += partition_size
+    return disc, occupied_partition_locations
+
+def calculate_checksum_2(disc):
+    checksum = 0
+    for disc_location in disc:
+        partition_id, partition_size = disc[disc_location]
+        if partition_id != '.':
+            for counter in range(0, partition_size):
+                checksum += partition_id * (disc_location + counter)
+    return checksum
+
+def find_empty_location_2(disc, n):
+    """First for a first available dot.
+
+    Start looking from n, not from beginning.
+    """
+    while True:
+        if disc[n] == '.':
+            return n
+        n += 1
 
 
 def solve2(data):
-    disc = {}
-    # id, len
-    counter = 0
-    id_counter = 0
-    myk = 0
-    all_num_keys = []
-    for e, i in enumerate(data_gen(data)):
-        if e % 2 == 0:
-            v = id_counter
-            id_counter += 1
-            all_num_keys.append(myk)
-        else:
-            v = '.'
-        disc[myk] = (v, i)
-        myk += i
+    disc, occupied_partition_locations = parse_data_to_dict_2(data)
 
-    for i in reversed(all_num_keys):
-        # print(i, disc[i])
-        myn, myval = disc[i]
-        target_k = None
-        for n  in sorted(disc.keys()):
-            val = disc[n]
-            if n >= i:
-                target_k = None
+    for disc_location_from_end in reversed(occupied_partition_locations):
+        partition_id, partition_size = disc[disc_location_from_end]
+        empty_disc_location = None
+
+        for disc_location_from_start  in sorted(disc.keys()):
+            tried_id, tried_size = disc[disc_location_from_start]
+
+            if disc_location_from_start >= disc_location_from_end:
+                empty_disc_location = None
                 break
-            elif val[0] == '.' and val[1] >= myval:
-                target_k = n
+            elif tried_id == '.' and tried_size >= partition_size:
+                empty_disc_location = disc_location_from_start
                 break
             else:
-                target_k = None
-        # print(target_k)
-        if target_k:
-            disc.pop(i)
-            foo_val = disc.pop(target_k)
-            disc[target_k] = (myn, myval)
-            if foo_val[1] > myval:
-                leng = foo_val[1] - myval
-                disc[target_k + leng + 1] = ('.', leng)
+                empty_disc_location = None
 
+        if empty_disc_location:
+            disc.pop(disc_location_from_end)
+            empty_location_id, empty_size = disc.pop(empty_disc_location)
+            disc[empty_disc_location] = (partition_id, partition_size)
+            if empty_size > partition_size:
+                # smaller empty partition needs to be inserted
+                new_size = empty_size - partition_size
+                disc[empty_disc_location + partition_size] = ('.', new_size)
 
-    # calculate checksum
-
-    checksum = 0
-    for k, v in disc.items():
-        if v[0] != '.':
-            x = v[1] #len
-            c = 0
-            while x:
-                checksum += v[0] * (k + c)
-                x -= 1
-                c += 1
-
-    #print(checksum)
-    #print(disc)
-    return checksum
-
-
-class Element:
-
-    def __init__(self, ch, ch_len, left=None, right=None):
-        self.ch = ch
-        self.ch_len = ch_len
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        return str(self.ch) * self.ch_len
-
-
+    return calculate_checksum_2(disc)
 
 
 if __name__ == '__main__':
