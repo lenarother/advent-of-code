640559d299fb1b14d591441f27d1fe8d7728f30a
Magdalena Rother <rother.magdalena@gmail.com>
2024-12-15 12:21:19 +0100

    Refactor day 14 2024

--- adventofcode_2024/day_14/solution.py
+++ adventofcode_2024/day_14/solution.py
@@ -4,6 +4,7 @@ https://adventofcode.com/2024/day/14
 
 """
 
+
 def parse_input(data):
     result = []
     for line in data.strip().split('\n'):
@@ -43,7 +44,7 @@ def move_multiple(p, v, n, p_max=(10, 6)):
     return p
 
 
-def calculate_is_in_q(p, p_min, p_max):
+def calculate_is_in_quadrant(p, p_min, p_max):
     x, y = p
     x_min, y_min = p_min
     x_max, y_max = p_max
@@ -52,27 +53,20 @@ def calculate_is_in_q(p, p_min, p_max):
         y_min <= y <= y_max
     )
 
-def calculate_n_in_q(positions, p_min, p_max):
-    return sum([calculate_is_in_q(p, p_min, p_max) for p in positions])
+
+def count_positions_in_quadrant(positions, p_min, p_max):
+    return sum([calculate_is_in_quadrant(p, p_min, p_max) for p in positions])
 
 
-def get_qs(p_max):
+def get_quadrants(p_max):
     x_max, y_max = p_max
     return [
-        ((0, 0), (x_max // 2 - 1, y_max // 2 -1)),
-        ((0, y_max // 2 + 1), (x_max // 2 -1, y_max)),
-        ((x_max // 2 + 1, 0), (x_max, y_max // 2 -1)),
+        ((0, 0), (x_max // 2 - 1, y_max // 2 - 1)),
+        ((0, y_max // 2 + 1), (x_max // 2 - 1, y_max)),
+        ((x_max // 2 + 1, 0), (x_max, y_max // 2 - 1)),
         ((x_max // 2 + 1, y_max // 2 + 1), (x_max, y_max)),
     ]
 
-def get_matrix(points, p_max):
-    x_max, y_max = p_max
-    picture = ''
-    for y in range(y_max + 1):
-        for x in range(x_max + 1):
-            picture += '#' if (x, y) in points else '.'
-        picture += '\n'
-    return picture
 
 def solve(data, n=100, p_max=(10, 6)):
     result = 1
@@ -81,43 +75,53 @@ def solve(data, n=100, p_max=(10, 6)):
     for i in data:
         new_p = move_multiple(i[0], i[1], n, p_max)
         final_positions.append(new_p)
-    for qp_min, qp_max in get_qs(p_max):
-        foo = calculate_n_in_q(final_positions, qp_min, qp_max)
-        result *= foo
+    for qp_min, qp_max in get_quadrants(p_max):
+        result *= count_positions_in_quadrant(final_positions, qp_min, qp_max)
 
     return result
 
+
+def print_picture(points, p_max):
+    x_max, y_max = p_max
+    picture = ''
+    for y in range(y_max + 1):
+        for x in range(x_max + 1):
+            picture += '#' if (x, y) in points else '.'
+        picture += '\n'
+    print(picture)
+    return picture
+
+
 def solve2(data, n=100, p_max=(10, 6)):
-    foo = 1
+    counter = 1
     data = parse_input(data)
 
-    picture = get_matrix([x[0] for x in data], p_max)
-    print(picture)
-    while foo < n:
+    print_picture([x[0] for x in data], p_max)
+    while counter < n:
         final_positions = []
         new_data = []
         for i in data:
             new_p = move(i[0], i[1], p_max)
             final_positions.append(new_p)
             new_data.append((new_p, i[1]))
-        picture = get_matrix(final_positions, p_max)
         if (
-            ((foo - 13) % 101 == 0) or
-            ((foo -65) % 103 == 0)
+            ((counter - 13) % 101 == 0) or
+            ((counter - 65) % 103 == 0)
         ):
             print('-' * 100)
-            print(foo)
+            print(counter)
             print('-' * 100)
-            print(picture)
+            print_picture(final_positions, p_max)
             print('-' * 100)
         data = new_data
-        foo += 1
+        counter += 1
 
 
 if __name__ == '__main__':
     input_data = open('input_data.txt').read()
+
     result = solve(input_data, 100, (100, 102))
     print(f'Example1: {result}')
 
-    result = solve2(input_data, 1000000, (100, 102))
+    result = solve2(input_data, 10_000, (100, 102))
     print(f'Example2: {result}')
