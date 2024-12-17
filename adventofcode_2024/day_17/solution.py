247f2a0c234a47970d3be22c17e38aca5a608b15
Magdalena Rother <rother.magdalena@gmail.com>
2024-12-17 12:52:02 +0100

    Day 17 2024 part 1

---
+++ adventofcode_2024/day_17/solution.py
@@ -0,0 +1,102 @@
+"""Day 17: Chronospatial Computer
+
+https://adventofcode.com/2024/day/17
+
+"""
+import re
+
+
+def get_register(data):
+    return {
+        'A': int(re.findall(r'Register A: (\d+)', data)[0]),
+        'B': int(re.findall(r'Register B: (\d+)', data)[0]),
+        'C': int(re.findall(r'Register C: (\d+)', data)[0]),
+    }
+
+
+def get_program(data):
+    return [int(i) for i in data.strip().split('\n\n')[1].replace('Program: ', '').split(',')]
+
+
+def literal(operand):
+    return operand
+
+
+def combo(operand, register):
+    if operand in range(0,4):
+        return operand
+    elif operand == 4:
+        return register['A']
+    elif operand == 5:
+        return register['B']
+    elif operand == 6:
+        return register['C']
+    elif operand == 7:
+        raise InterruptedError
+
+
+def instruction(opcode, operand, register, instruction_pointer):
+    # adv
+    if opcode == 0:
+        register['A'] = int(register['A'] / (2 ** combo(operand, register)))
+
+    # bxl
+    elif opcode == 1:
+        register['B'] = register['B'] ^ literal(operand)
+
+    # bst
+    elif opcode == 2:
+        register['B'] = int(str((combo(operand, register) % 8))[-3:])
+
+    # jnz
+    elif opcode == 3:
+        if register['A'] == 0:
+            pass
+        else:
+            return None, literal(operand)
+    # bxc
+    elif opcode == 4:
+        register['B'] = register['B'] ^ register['C']
+
+    # out
+    elif opcode == 5:
+        return combo(operand, register) % 8, instruction_pointer + 2
+
+    # bvd
+    elif opcode == 5:
+        return combo(operand, register) % 8, instruction_pointer + 2
+
+    # bdv
+    if opcode == 6:
+        register['B'] = int(register['A'] / (2 ** combo(operand, register)))
+
+    # cdv
+    if opcode == 7:
+        register['C'] = int(register['A'] / (2 ** combo(operand, register)))
+
+    return None, instruction_pointer + 2
+
+
+def solve(data):
+    register = get_register(data)
+    program = get_program(data)
+    instruction_pointer = 0
+    outputs = []
+    while instruction_pointer < len(program):
+        val, instruction_pointer = instruction(
+            program[instruction_pointer],
+            program[instruction_pointer + 1],
+            register,
+            instruction_pointer
+        )
+        if val is not None:
+            outputs.append(val)
+
+    result = ','.join([str(i) for i in outputs])
+    return result
+
+
+if __name__ == '__main__':
+    input_data = open('input_data.txt').read()
+    result = solve(input_data)
+    print(f'Example1: {result}')
---
+++ adventofcode_2024/day_17/test_solution.py
@@ -0,0 +1,29 @@
+import pytest
+
+from .solution import solve
+
+DATA = """
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+"""
+
+DATA_2 = """
+Register A: 10
+Register B: 0
+Register C: 0
+
+Program: 5,0,5,1,5,4
+"""
+
+EXAMPLES = (
+    (DATA, "4,6,3,5,6,3,5,2,1,0"),
+    (DATA_2, "0,1,2"),
+)
+
+
+@pytest.mark.parametrize('data,expected', EXAMPLES)
+def test_solve(data, expected):
+    assert solve(data) == expected
