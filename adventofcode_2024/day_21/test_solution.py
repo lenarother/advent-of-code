12cd03cd3cf0f59fc881cd9b15dfdbc44ab10a97
Magdalena Rother <rother.magdalena@gmail.com>
2024-12-21 22:02:34 +0100

    Day 21 2024 part 1 ugly

--- adventofcode_2024/day_21/test_solution.py
+++ adventofcode_2024/day_21/test_solution.py
@@ -9,6 +9,7 @@ DATA = """
 456A
 379A
 """
+
 EXAMPLES = (
     (DATA, 126384),
     ('029A', 1972),
@@ -21,26 +22,3 @@ EXAMPLES = (
 @pytest.mark.parametrize('data,expected', EXAMPLES)
 def test_solve(data, expected):
     assert solve(data) == expected
-
-
-
-
-#<vA<AA>^>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-#v <<A>>^A<A>AvA<^AA>A<vAAA>^A
-
-#+---+---+---+
-#| 7 | 8 | 9 |
-#+---+---+---+
-#| 4 | 5 | 6 |
-#+---+---+---+
-#| 1 | 2 | 3 |
-#+---+---+---+
-#    | 0 | A |
-#    +---+---+
-
-
-#    +---+---+
-#    | ^ | A |
-#+---+---+---+
-#| < | v | > |
-#+---+---+---+
